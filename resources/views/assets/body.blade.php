96245e3615eb3bb82a9a46548a7dff2e2f963a83
Elko Aditya <elkoadp@gmail.com>
2023-06-13 22:51:41 +0700

    update show modal images

--- resources/views/assets/body.blade.php
+++ resources/views/assets/body.blade.php
@@ -114,7 +114,7 @@
 
 <!-- BEGIN: Footer-->
 <footer class="footer footer-static footer-light">
-    <p class="clearfix mb-0"><span class="float-md-start d-block d-md-inline-block mt-25">COPYRIGHT &copy; 2022<a class="ms-25" href="https://starko.biz" target="_blank">STARKO.BIZ</a><span class="d-none d-sm-inline-block">, All rights Reserved</span></span><span class="float-md-end d-none d-md-block">Created & Disdained with <i data-feather="heart"></i></span></p>
+
 </footer>
 <button class="btn btn-primary btn-icon scroll-top" type="button"><i data-feather="arrow-up"></i></button>
 <script src="{{ asset('vuexy') }}/app-assets/vendors/js/vendors.min.js"></script>
